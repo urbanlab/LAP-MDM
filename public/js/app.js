57be6f94a8d7fa515df13b25ee7bca8c8a68ec14
methio <thomas.perivolas@gmail.com>
2019-03-25 15:31:44 +0100

    Added gitignore, Merge css

--- public/js/app.js
+++ public/js/app.js
@@ -49180,8 +49180,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/Elodie/Documents/ALTERNANCE/MDMRemix/workshop/LAP_MDM/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/Elodie/Documents/ALTERNANCE/MDMRemix/workshop/LAP_MDM/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/methio/Documents/COURS/P5-remix/LAP-MDM/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/methio/Documents/COURS/P5-remix/LAP-MDM/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
